0817735476387decd0566ff9ca53c84174b89150
opeyemi <omotayoopeyemi32@gmail.com>
2023-09-11 09:59:45 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,25 +2,24 @@
 /**
  * main - main block here
  * Description - Numbers must be separated by ,, followed by a space
+ * return - 0
  */
 int main(void)
 {
-    int i, j;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar(i + '0');
-            putchar(j + '0');
-
-            if (i < 8 || j < 9)
-            {
-            putchar(',');
-            putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = i + 1; j <= 9; j++)
+{
+putchar(i + '0');
+putchar(j + '0');
+if (i < 8 || j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
