50f215c77a21314f38e7b0d5aaebc814622fb09a
opeyemi <omotayoopeyemi32@gmail.com>
2023-09-11 18:43:46 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 /**
- * main - main block here
- * @parameter: i and j is an intergal 
- * Description - Numbers must be separated by ,, followed by a space
- * return - 0
+ * main - Prints the numbers from 00 to 99, numbers separated by
+ *        a comma followed by a space, in ascending order.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, j;
-for (i = 0; i <= 9; i++)
-{
-for (j = i + 1; j <= 9; j++)
-{
-putchar(i + '0');
-putchar(j + '0');
-if (i < 8 || j < 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-putchar('\n');
-return (0);
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 10; digit1++)
+	{
+		for (digit2 = 0; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
