bfe72ffb8a150f042fd8d91d72ae70cfa3447e5a
opeyemi <omotayoopeyemi32@gmail.com>
2023-09-13 06:12:49 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _abs - Print the absolute value of an integer.
+ *@n - The character to be check
+ * Return: the absolute value of an integer.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (-n);
+	}
+}
